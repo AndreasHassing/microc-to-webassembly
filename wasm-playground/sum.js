e7d5c3bb6e6e2a9bb85e3841cacc97300da7eebe
AndreasHassing <andreas@famhassing.dk>
2017-03-17 20:05:44 +0100

    Update sum WASM sample

--- wasm-playground/sum.js
+++ wasm-playground/sum.js
@@ -1,41 +1,51 @@
-// global object for the sum function
-let sum
-
-fetch('sum.wasm').then(response =>
-  response.arrayBuffer()
-).then(buffer =>
-  WebAssembly.instantiate(buffer)
-).then(({module, instance}) =>
-  sum = instance.exports.sum
-).then(() => runTests())
-
 // JavaScript equivalent to `sum.wast`
-function sumJs(x, n) {
-  let sum = 0;
-  while (n-- != 0) {
-    sum = sum + x;
-  }
-  return sum;
+function sumJs(a, b) {
+	let sum = 0
+	while (a != b) {
+		sum = sum + a
+		a += 1
+	}
+	return sum + a
 }
 
-function runTests() {
-  // test iterations
-  let ti = 3
-  // number to exponentiate
-  let x = 4
-  // exponent
-  let n = 1000000000
+async function getWasmSumFunc() {
+	const binary = await fetch('sum.wasm')
+	const bytes = await binary.arrayBuffer()
+	const module = await WebAssembly.compile(bytes)
+	const instance = await WebAssembly.instantiate(module)
 
-  // ti = test iterations
-  while (ti > 0) {
-    console.time('wasm')
-    sum(x, n)
-    console.timeEnd('wasm')
+	return instance.exports.sum
+}
 
-    console.time('js')
-    sumJs(x, n)
-    console.timeEnd('js')
+getWasmSumFunc().then((wasmSumFunc) => {
+	runTests(sumJs, wasmSumFunc)
+})
 
-    ti--
-  }
+function runTests(jsSumFunc, wasmSumFunc) {
+	// test iterations
+	const ti = 50000000
+
+	// sum from a to and including b
+	let a = 1, b = 50
+
+	let wasmTime = 0
+	let jsTime = 0
+
+	let testCounter = ti
+	while (testCounter > 0) {
+		let wasmTimeBefore = Date.now()
+		wasmSumFunc(a, b)
+		let wasmTimeAfter = Date.now()
+		wasmTime += (wasmTimeAfter - wasmTimeBefore)
+
+		let jsTimeBefore = Date.now()
+		jsSumFunc(a, b)
+		let jsTimeAfter = Date.now()
+		jsTime += (jsTimeAfter - jsTimeBefore)
+
+		testCounter--
+	}
+
+	console.log(`wasm time: ${wasmTime}`)
+	console.log(`js time: ${jsTime}`)
 }
--- wasm-playground/sum.wast
+++ wasm-playground/sum.wast
@@ -1,14 +1,14 @@
-(module
-  (func $sum (export "sum") (param i32 i32) (result i32)
-    (local i32)
-    (block
-      (loop
-        (br_if 1 (i32.eqz (get_local 1)))
-        (set_local 2 (i32.add (get_local 2) (get_local 0)))
-        (set_local 1 (i32.sub (get_local 1) (i32.const 1)))
-        (br 0)
+(module                                                     ;; specify a module
+  (func $sum (export "sum") (param i32 i32) (result i32)    ;; define and export a function called "sum"
+    (local i32)                                             ;; define a local variable (sum)
+    (block                                                  ;; start a new loop block
+      (loop                                                 ;; define a loop
+        (br_if 1 (i32.eq (get_local 0) (get_local 1)))      ;; branch out of loop if a = b
+        (set_local 2 (i32.add (get_local 2) (get_local 0))) ;; sum = sum + a
+        (set_local 0 (i32.add (get_local 0) (i32.const 1))) ;; a = a + 1
+        (br 0)                                              ;; branch to top of loop
       )
     )
-    (get_local 2)
+    (i32.add (get_local 2) (get_local 0))                   ;; add sum to stack (and return the function)
   )
 )
