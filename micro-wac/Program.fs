25d8682e2ddbb8732778435358378f617cdcc23a
Andreas Hassing <andreas@famhassing.dk>
2017-04-03 20:34:46 +0200

    Migrate to solution based project style

---
+++ micro-wac/Program.fs
@@ -0,0 +1,9 @@
+﻿[<EntryPoint>]
+let main argv =
+    printfn "Hello there, compiling your stuff"
+    printfn "%A" argv
+    printfn "Done compiling your stuff LOL"
+
+    // pause until user presses the enter key
+    System.Console.ReadLine() |> ignore
+    0 // return an integer exit code
