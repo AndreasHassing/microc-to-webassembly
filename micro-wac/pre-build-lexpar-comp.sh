a791b912adce737aa14646239aea3806ade46573
AndreasHassing <andreas@famhassing.dk>
2017-05-03 21:39:21 +0200

    Improve lexer and parser for cross platforms (.sh *nix version)

--- micro-wac/pre-build-lexpar-comp.sh
+++ micro-wac/pre-build-lexpar-comp.sh
@@ -5,42 +5,46 @@ parserFile="CPar.fsy"
 lexerHashFile=".lexer.compiled.hash"
 parserHashFile=".parser.compiled.hash"
 
+GetFileHash() {
+	echo -n $(git hash-object $1)
+}
+
 AlreadyCompiledLexerAndParser() {
-    if [ ! -f $lexerHashFile ] || [ ! -f $parserHashFile ]; then
-        # 0=true, 1=false
-        return 1
-    fi
+	if [ ! -f $lexerHashFile ] || [ ! -f $parserHashFile ]; then
+		# 0=true, 1=false
+		return 1
+	fi
 
-    lexerHash=$(md5 < "${lexerFile}" | tr '[:lower:]' '[:upper:]')
-    parserHash=$(md5 < "${parserFile}" | tr '[:lower:]' '[:upper:]')
+	lexerHash=$(GetFileHash "$lexerFile")
+	parserHash=$(GetFileHash "$parserFile")
 
-    existingLexerHash=$(cat "${lexerHashFile}")
-    existingParserHash=$(cat "${parserHashFile}")
+	existingLexerHash=$(cat "${lexerHashFile}")
+	existingParserHash=$(cat "${parserHashFile}")
 
-    if [ $lexerHash = $existingLexerHash ] && [ $parserHash = $existingParserHash ]; then
-        return 0
-    else
-        return 1
-    fi
+	if [ $lexerHash = $existingLexerHash ] && [ $parserHash = $existingParserHash ]; then
+		return 0
+	else
+		return 1
+	fi
 }
 
 GenerateLexerAndParserHash() {
-    md5 < "${lexerFile}" | tr '[:lower:]' '[:upper:]' > "${lexerHashFile}"
-    md5 < "${parserFile}" | tr '[:lower:]' '[:upper:]' > "${parserHashFile}"
+	GetFileHash $lexerFile > "${lexerHashFile}"
+	GetFileHash $parserFile > "${parserHashFile}"
 }
 
 CompileLexerAndParser() {
-    fslexyaccPath=$(echo mono ../packages/$(ls ../packages/ | grep "FsLexYacc.\d")/build/)
-    fsyaccCmd="${fslexyaccPath}fsyacc.exe --module CPar CPar.fsy -o CPar.fs"
-    fslexCmd="${fslexyaccPath}fslex.exe --unicode CLex.fsl -o CLex.fs"
+	fslexyaccPath=$(echo mono ../packages/$(ls ../packages/ | grep "FsLexYacc.\d")/build/)
+	fsyaccCmd="${fslexyaccPath}fsyacc.exe --module CPar CPar.fsy -o CPar.fs"
+	fslexCmd="${fslexyaccPath}fslex.exe --unicode CLex.fsl -o CLex.fs"
 
-    eval $fsyaccCmd
-    eval $fslexCmd
+	eval $fsyaccCmd
+	eval $fslexCmd
 }
 
 if ! AlreadyCompiledLexerAndParser; then
-    CompileLexerAndParser
-    GenerateLexerAndParserHash
+	CompileLexerAndParser
+	GenerateLexerAndParserHash
 else
-    echo "Parser and lexer is already compiled"
+	echo "Parser and lexer is already compiled"
 fi
