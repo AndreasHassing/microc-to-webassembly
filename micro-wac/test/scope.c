f2eea6795d73dc490b4a5ceffbab115e0325ccaf
Andreas Hassing <andreas@famhassing.dk>
2017-05-12 10:16:39 +0200

    Add scoping test sample

---
+++ micro-wac/test/scope.c
@@ -0,0 +1,32 @@
+void start() {
+	int x;
+
+	{
+		int x;
+		x = 5;
+		print x;
+		println;
+	}
+
+	{
+		int x;
+		print x; // should print 0
+		println;
+		x = 3;
+		print x;
+		println;
+	}
+
+	{
+		int y;
+		y = 42;
+		print y;
+		println;
+	}
+
+	print x; // should print 0
+	println;
+
+	x = 7;
+	print x;
+}
